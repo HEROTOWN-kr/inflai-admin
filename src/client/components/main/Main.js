383d4c1605ed7a328d3ae7658a2e66d05c720688
Andrian <andriantsoy@gmail.com>
2020-04-16 18:58:41 +0900

    결제 리스트 출력

--- src/client/components/main/Main.js
+++ src/client/components/main/Main.js
@@ -8,15 +8,45 @@ import DraftsIcon from '@material-ui/icons/Drafts';
 import { Route, Switch, Link } from 'react-router-dom';
 import Advertiser from './advertiser/Advertiser';
 import Influencer from './influencer/Influencer';
+import Payment from './payment/Payment';
 
 function Main(props) {
+  const links = [
+    {
+      name: '광고주',
+      link: '/Advertiser'
+    },
+    {
+      name: '인플루언서',
+      link: '/Influencer'
+    },
+    {
+      name: '판매관리 (결제관리)',
+      link: '/Payment'
+    },
+  ];
+
+
   return (
     <Grid container className="main" alignItems="center">
       <Grid item md={2}>
         <Grid container>
           <Grid item md={12}>
             <List component="nav" aria-label="main mailbox folders">
-              <ListItem
+              {links.map(item => (
+                <ListItem
+                  key={item.name}
+                  button
+                      // key={link.text}
+                  component={renderProps => <Link to={props.match.path + item.link} {...renderProps} />}
+                >
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={item.name} />
+                </ListItem>
+              ))}
+              {/* <ListItem
                 button
                 // key={link.text}
                 component={renderProps => <Link to={`${props.match.path}/Advertiser`} {...renderProps} />}
@@ -35,7 +65,7 @@ function Main(props) {
                   <DraftsIcon />
                 </ListItemIcon>
                 <ListItemText primary="인플루언서" />
-              </ListItem>
+              </ListItem> */}
             </List>
           </Grid>
         </Grid>
@@ -51,6 +81,11 @@ function Main(props) {
             path={`${props.match.path}/Influencer`}
           // render={props => <ProductMix {...props} user={user} changeUser={changeUser} />}
             render={renderProps => <Influencer {...renderProps} />}
+          />
+          <Route
+            path={`${props.match.path}/Payment`}
+              // render={props => <ProductMix {...props} user={user} changeUser={changeUser} />}
+            render={renderProps => <Payment {...renderProps} />}
           />
         </Switch>
       </Grid>
